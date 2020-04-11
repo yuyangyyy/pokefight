e1e38a6289538de26fbf3ae3ec7ca9b825c7e24e
orko3012 <or.ko@hotmail.fr>
2020-04-11 18:03:16 +0200

    18-Form add first name text area

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./Form.css";
 
-class Form extends React.Component{
-    render(){
-        return(
-            
-        )
-    }
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+    };
+    this.changeName = this.changeName.bind(this);
+  }
+  changeName(event) {
+    this.setState({ firstName: event.target.value });
+  }
+
+  render() {
+    return (
+      <form className="form">
+        <label htmlFor="name">First name:</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={this.state.firstName}
+          onChange={this.changeName}
+        />
+      </form>
+    );
+  }
 }
 
 export default Form;
