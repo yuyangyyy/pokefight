da595fe4078aab4661e5b08d11c7c24726380616
Julien Rousseau <julienrousseau16@gmail.com>
2020-04-14 11:50:28 +0200

    images folder added, title imported in Intro.js

--- src/1. pages/Intro.js
+++ src/1. pages/Intro.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import title from '../3. images/Poketitre.png'
+import './Intro.css'
 
 const Intro = () =>{
     return (
         <div>
-
+            <img src={title} alt="title"/>
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 //import './App.css';
+import Intro from './1. pages/Intro'
 
 function App() {
   return (
     <div className="App">
-      
+      <Intro />
     </div>
   );
 }
