4065bf99d9a39f80521d0702ab30a1b2ad72c497
Yang YU <yuyangyyy@gmail.com>
2020-04-14 16:06:44 +0200

    Start the 1-navbar task

---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="sousnavbar">
+        <div className="logo">PokeFight</div>
+
+        <div className="links">
+          <ul>
+            <li className="new-game">
+              <a href="#Nouvellepartie">New game</a>
+            </li>
+
+            <li className="pokedex">
+              <a href="#PokéDex">PokéDex</a>
+            </li>
+
+            <li className="classement">
+              <a href="#Classement">Ranking</a>
+            </li>
+
+            <li className="contact">
+              <a href="#Contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
