fb87bf119d4ffa07ccd21923363c054526c1a5e5
Julien Rousseau <julienrousseau16@gmail.com>
2020-04-14 11:59:24 +0200

    spinning pokeball imported, minimum css added

--- src/1. pages/Intro.js
+++ src/1. pages/Intro.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import title from '../3. images/Poketitre.png'
+import ball from '../3. images/Pokeball.png'
 import './Intro.css'
 
 const Intro = () =>{
     return (
         <div>
-            <img src={title} alt="title"/>
+            <img className='titre' src={title} alt="title"/>
+            <img className='pokeball' src={ball} alt='pokeball'/>
         </div>
     )
 }
