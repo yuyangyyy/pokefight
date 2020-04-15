015ead7287033017daa8ebeee41e4098fc0bf10e
Julien Rousseau <julienrousseau16@gmail.com>
2020-04-15 18:24:31 +0200

    countdown functional

--- src/1. pages/Transition.js
+++ src/1. pages/Transition.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import './Transition.css'
 
-const Transition = () => {
-    return (
-        <div>
+class Transition extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 5
+        }
+    }
 
-        </div>
-    )
+    render() {
+        const { count } = this.state
+        return (
+            <div className='container'>
+                <div className='left'></div>
+                <div className='white'><div id='count'>{count}</div></div>
+                <div className='right'></div>
+            </div>
+        )
+    }
+    componentDidMount() {
+        this.countDown =
+            setInterval(() => {
+                if (this.state.count > 0) {
+                    this.setState({ count: this.state.count - 1 })
+                }
+            }, 1000)
+    }
+
 }
 
 export default Transition
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 //import './App.css';
-import Intro from './1. pages/Intro'
+import Transition from './1. pages/Transition';
 
 function App() {
   return (
     <div className="App">
-      <Intro />
+      <Transition />
     </div>
   );
 }
