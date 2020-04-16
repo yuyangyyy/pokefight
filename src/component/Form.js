67adda0486eaa6dd7100ff56e19b509c719f3d60
orko3012 <or.ko@hotmail.fr>
2020-04-16 11:30:11 +0200

    18-Form change style and responsive

--- src/component/Form.js
+++ src/component/Form.js
@@ -19,50 +19,69 @@ class Form extends React.Component {
   render() {
     const { firstName, lastName, email, message } = this.state;
     return (
-      <div id="container">
+      <div>
+        <div id="empty"></div>
         <form className="form">
           <div className="formContainer">
-            <label htmlFor="firstName">First name:</label>
-            <input
-              id="firstName"
-              className="text"
-              name="firstName"
-              type="text"
-              value={firstName}
-              onChange={this.changeInput}
-            />
+            <div className="label">
+              <label htmlFor="firstName" id="first">
+                First name:
+              </label>
+            </div>
+            <div className="label">
+              <input
+                id="firstName"
+                className="text"
+                name="firstName"
+                type="text"
+                value={firstName}
+                onChange={this.changeInput}
+              />
+            </div>
           </div>
           <div className="formContainer">
-            <label htmlFor="lastName">Last name:</label>
-            <input
-              id="lastName"
-              className="text"
-              name="lastName"
-              type="text"
-              value={lastName}
-              onChange={this.changeInput}
-            />
+            <div className="label">
+              <label htmlFor="lastName">Last name:</label>
+            </div>
+            <div className="label">
+              <input
+                id="lastName"
+                className="text"
+                name="lastName"
+                type="text"
+                value={lastName}
+                onChange={this.changeInput}
+              />
+            </div>
           </div>
           <div className="formContainer">
-            <label htmlFor="email">Email:</label>
-            <input
-              id="email"
-              className="text"
-              name="email"
-              type="email"
-              value={email}
-              onChange={this.changeInput}
-            />
+            <div className="label">
+              <label htmlFor="email">Email:</label>
+            </div>
+            <div className="label">
+              <input
+                id="email"
+                className="text"
+                name="email"
+                type="email"
+                value={email}
+                onChange={this.changeInput}
+              />
+            </div>
           </div>
           <div className="formContainer">
-            <label htmlFor="message">Your message:</label>
-            <textarea
-              id="message"
-              className="text"
-              name="message"
-              value={message}
-              onChange={this.changeInput}
-            ></textarea>
+            <div className="label">
+              <label htmlFor="message">Your message:</label>
+            </div>
+            <div className="label">
+              <textarea
+                id="message"
+                className="text"
+                name="message"
+                value={message}
+                onChange={this.changeInput}
+              ></textarea>
+            </div>
           </div>
           <input id="button" type="submit" value="Submit" />
         </form>
