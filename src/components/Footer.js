260e79405acb18b198c9ba65348afbd3c8ba1b89
Walid <wmahersia@gmail.com>
2020-04-17 10:59:22 +0200

    responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../LogoPoke.png';
+// import logobis from '../LogoPokeBlanc.png';
 import logoFb from '../logo-fb.svg';
 import logoTwitter from '../logo_twitter.svg';
 import logoYoutube from '../logo_yt.svg';
@@ -18,23 +19,27 @@ class Footer extends React.Component {
 
                     <div className="flex-list">
                         <ul>
-                            <li>Contact Us</li>
-                            <li>Sponsors</li>
+                            <li> <a href="R" >Contact Us</a></li>
+                            <li><a href="R" >Sponsors</a></li>
                         </ul>
                         <ul>
-                            <li>Legal Mention</li>
-                            <li>Nintendo France</li>
+                            <li><a href="R" >Legal Mentions</a></li>
+                            <li><a href="R" >Nintendo France</a></li>
                         </ul>
                     </div>
 
-                    <hr></hr>
+                    <hr />
                     <img className="logo-poke" src={logo} alt="Logo pokemon" />
-                    <hr></hr>
+                    <hr />
 
-                    <div className="flex-logoReseau"> 
-                        <img className="logo-fb" src={logoFb} alt="Logo facebook" />
-                        <img className="logo-twitter" src={logoTwitter} alt="Logo twitter" />
-                        <img className="logo-youtube" src={logoYoutube} alt="Logo youtube" />
+                    <div className="flex-logoReseau">
+                        <a href="https://www.facebook.fr" target="_blank" rel="noopener noreferrer"><img className="logo-fb" src={logoFb} alt="Logo facebook" /></a>
+
+
+                        <a href="https://twitter.com/explore" target="_blank" rel="noopener noreferrer"><img className="logo-twitter" src={logoTwitter} alt="Logo twitter" /></a>
+
+
+                        <a href="https://www.youtube.fr" target="_blank" rel="noopener noreferrer"> <img className="logo-youtube" src={logoYoutube} alt="Logo youtube" /> </a>
                     </div>
 
 
