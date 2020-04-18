b540205740de9ca090b2d64d7351057267ac7a19
Julien Rousseau <julienrousseau16@gmail.com>
2020-04-18 18:29:45 +0200

    first steps with router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 import React from "react";
-import DialogBox from './components/DialogBox';
-import Footer from './components/Footer'
-import Form from "./components/Form";
+// import DialogBox from "./components/DialogBox";
+import Footer from "./components/Footer";
+// import Form from "./components/Form";
 import Navbar from "./components/Navbar";
-import Potion from './components/Potion'
-import Ranking from './components/Ranking'
-import Transition from './components/Transition';
-import './App.css'
+// import Potion from "./components/Potion";
+// import Ranking from "./components/Ranking";
+// import Transition from "./components/Transition";
 
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="prez-diagbox">
-        <Potion />
-        <DialogBox textDialog="Welcome to the world of PokeFight! Start by entering your username" idDialogBox="pseudoPlayer1" firstButton="Confirm" placeholder="Player 1"/>
-        <DialogBox textDialog="Who do you want to fight ?!" firstButton="Computer" scndButton="Friend"/>
-        <DialogBox textDialog="Enter your friend's name :" idDialogBox="pseudoPlayer2" firstButton="Confirm" placeholder="Player 2"/>
-        <DialogBox textDialog="Now, Player1 choose your Pokémon!" dialogSize="280px" />
+      <div className="App">
+        <Navbar />
+        <Footer />
+        {/*
+        <div className="prez-diagbox">
+          <Potion />
+          <DialogBox
+            textDialog="Welcome to the world of PokeFight! Start by entering your username"
+            idDialogBox="pseudoPlayer1"
+            firstButton="Confirm"
+            placeholder="Player 1"
+          />
+          <DialogBox
+            textDialog="Who do you want to fight ?!"
+            firstButton="Computer"
+            scndButton="Friend"
+          />
+          <DialogBox
+            textDialog="Enter your friend's name :"
+            idDialogBox="pseudoPlayer2"
+            firstButton="Confirm"
+            placeholder="Player 2"
+          />
+          <DialogBox
+            textDialog="Now, Player1 choose your Pokémon!"
+            dialogSize="280px"
+          />
+        </div>
+        <Ranking />
+        <Transition />
+        <Form />
+        <Footer /> */}
       </div>
-      <Ranking />
-      <Transition />
-      <Form />
-      <Footer/>
-
-    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,52 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../img/logo/LogoPokeBlanc.png";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import DialogBox from "./DialogBox";
+import Form from "./Form";
+import Potion from "./Potion";
+import Ranking from "./Ranking";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="sousnavbar">
-        <img className="navlogo" src={logo} alt="logoPokemon" />
+    <Router>
+      <div className="page-container">
+        <div className="navbar">
+          <div className="sousnavbar">
+            <Link to="/">
+              <img className="navlogo" src={logo} alt="logoPokemon" />
+            </Link>
 
-        <div className="links">
-          <ul>
-            <li className="new-game">
-              <a href="#Nouvellepartie">New game</a>
-            </li>
+            <div className="links">
+              <ul>
+                <li className="new-game">
+                  <Link to="/new-game">New Game</Link>
+                </li>
 
-            <li className="pokedex">
-              <a href="#PokéDex">PokéDex</a>
-            </li>
+                <li className="pokedex">
+                  <Link to="/pokedex">Pokedex</Link>
+                </li>
 
-            <li className="classement">
-              <a href="#Classement">Ranking</a>
-            </li>
+                <li className="classement">
+                  <Link to="/ranking">Ranking</Link>
+                </li>
 
-            <li className="contact">
-              <a href="#Contact">Contact</a>
-            </li>
-          </ul>
+                <li className="contact">
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <Switch>
+          <Route exact path="/" />
+          <Route exact path="/new-game" component={DialogBox} />
+          <Route exact path="/pokedex" component={Potion} />
+          <Route path="/ranking" component={Ranking} />
+          <Route path="/contact" component={Form} />
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
