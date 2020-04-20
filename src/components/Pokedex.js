06845b1340825bcf790552c5aec950658b64a8e6
Auxence <auxence.blondel@gmail.com>
2020-04-21 00:55:14 +0200

    add search filter

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -3,81 +3,81 @@ import axios from 'axios'
 import './Pokedex.css'
 
 const propComparator = (propName) =>
-  (a, b) => a[propName] == b[propName] ? 0 : a[propName] < b[propName] ? -1 : 1
+    (a, b) => a[propName] == b[propName] ? 0 : a[propName] < b[propName] ? -1 : 1
 
 class Pokedex extends React.Component {
     state = {
         nbPokemons: 105,
         pokemons: [],
         showMore: false,
-        nbShow: 10
+        nbShow: 12,
+        search: ""
     }
 
     fetchPokemon = () => {
         for (let i = 1; i < this.state.nbPokemons; i++) {
             let url = `https://pokeapi.co/api/v2/pokemon/${i}`
             axios.get(url)
-                .then(response => {  
+                .then(response => {
                     let arrPokemons = [...this.state.pokemons, response.data]
-                    this.setState({pokemons: arrPokemons}) 
-                    console.log(i)
-                    if(i === 9){
-                        console.log(this.state.pokemons.sort(propComparator("id")))
-                    }
+                    this.setState({ pokemons: arrPokemons })
+                    this.state.pokemons.sort(propComparator("id"))
                 })
-        }        
+        }
     }
 
-    handleClick = () =>{
-        this.setState({showMore: true})
-        // let prevState = this.state.nbPokemons
-        // this.setState({nbPokemons: prevState+=13})
-        // this.fetchPokemon()
+    handleClick = () => {
+        this.setState({ showMore: true })
     }
 
-    componentDidUpdate(prevState){
-        // console.log(prevState.pok)
+    handleChange = (event) => {
+        this.setState({search: event.target.value})
+        console.log(this.state.search)
     }
 
     componentDidMount() {
         this.fetchPokemon()
     }
 
+    componentDidUpdate(){
+
+    }
+
     render() {
-        const { pokemons, showMore, nbShow } = this.state
-        // const numberOfItems = showMore ? nbShow+=10 : 10
-        if(showMore){
-            let newNbShow = nbShow + 10
-            this.setState({nbShow: newNbShow})
-            this.setState({showMore: false})
-        }
+        const { nbPokemons, pokemons, showMore, nbShow, search } = this.state
+        if (showMore)
+            this.setState({ nbShow: nbShow + 12, showMore: false })
+
         return (
             <div>
+                <div className="filter-pokemon">
+                    <input type="search" onChange={this.handleChange} value={search}/>    
+                </div>
                 <div className="Pokedex">
-                    
-                    {pokemons && pokemons.slice(0, nbShow)
-                    .map((pokemon, id) => {
-                        let pokemonCard =
-                            <div className="pokemon-card" key={id}>
-                                <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`} alt="" />
-                                <div className="pokemon-info">
-                                    <h3>{pokemon.name}</h3> 
-                                    <p className={pokemon.types[0].type.name + " pokemon-type"}>{pokemon.types[0].type.name}</p>
-                                    <p className={pokemon.types[1] && pokemon.types[1].type.name + " pokemon-type"}>{pokemon.types[1] && pokemon.types[1].type.name}</p>
-                                    
-                                </div>
-                            </div>
+                    {pokemons && pokemons.slice(0, search !== "" ? nbPokemons : nbShow)
+                        .filter(pokemon => pokemon.name.toLowerCase().includes(this.state.search.toLowerCase()))
+                        .map((pokemon, id) => {
+                            let pokemonCard =
+                                <div className="pokemon-card" key={id}>
+                                    <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`} alt="" />
+                                    <div className="pokemon-info">
+                                        <h3>{pokemon.name}</h3>
+                                        <p className={pokemon.types[0].type.name + " pokemon-type"}>{pokemon.types[0].type.name}</p>
+                                        <p className={pokemon.types[1] && pokemon.types[1].type.name + " pokemon-type"}>{pokemon.types[1] && pokemon.types[1].type.name}</p>
 
-                        return pokemonCard
-                    })}
-                    
+                                    </div>
+                                </div>
+
+                            return pokemonCard
+                        })}
+
                 </div>
                 <div className="show-more">
-                    <button onClick={this.handleClick}>Show more...</button> 
+                    <button onClick={this.handleClick}>Show more...</button>
                 </div>
             </div>
         )
     }
 }
 
-export default Pokedex
+export default Pokedex 
