f5997e9f41c06670e314e0f3e73aa6a4c2eb8de7
Auxence <auxence.blondel@gmail.com>
2020-04-21 21:27:10 +0200

    add algo of filter type and generation

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import axios from 'axios'
 import './Pokedex.css'
+import SortBox from './SortBox'
 
 const propComparator = (propName) =>
     (a, b) => a[propName] == b[propName] ? 0 : a[propName] < b[propName] ? -1 : 1
 
 class Pokedex extends React.Component {
-    state = {
-        nbPokemons: 105,
-        pokemons: [],
-        showMore: false,
-        nbShow: 12,
-        search: ""
+    constructor(props) {
+        super(props)
+        this.state = {
+            nbPokemons: 387,
+            pokemons: [],
+            arrPokemonsType: [],
+            showMore: false,
+            nbShow: 12,
+            pokemonSearch: "",
+            pokemonsGene: "",
+            pokemonsType: "",
+            sortGene: false,
+            sortType: false
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    fetchPokemon = () => {
+    getPokemons = () => {
         for (let i = 1; i < this.state.nbPokemons; i++) {
             let url = `https://pokeapi.co/api/v2/pokemon/${i}`
             axios.get(url)
@@ -26,36 +36,53 @@ class Pokedex extends React.Component {
         }
     }
 
+    getPokemonsType = () => {
+        axios.get("https://pokeapi.co/api/v2/type")
+            .then(response => response.data.results.slice(0, 17))
+            .then(data => {
+                this.setState({ arrPokemonsType: data.map(type => type.name) })
+            })
+    }
+
+
     handleClick = () => {
         this.setState({ showMore: true })
     }
-
+   
     handleChange = (event) => {
-        this.setState({search: event.target.value})
-        console.log(this.state.search)
+        const idName = event.target.id
+        console.log(idName)
+        this.setState({[idName]: event.target.value}) 
+        
     }
 
     componentDidMount() {
-        this.fetchPokemon()
+        this.getPokemons()
+        this.getPokemonsType()
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
 
     }
 
     render() {
-        const { nbPokemons, pokemons, showMore, nbShow, search } = this.state
+        const arrPokemonGene =["1st Generation", "2nd Generation", "3rd Generation"]
+
+        const { nbPokemons, arrPokemonsType, pokemonsType, pokemons, showMore, nbShow, pokemonSearch, pokemonsGene } = this.state
         if (showMore)
             this.setState({ nbShow: nbShow + 12, showMore: false })
-
         return (
             <div>
                 <div className="filter-pokemon">
-                    <input type="search" onChange={this.handleChange} value={search}/>    
+                    <SortBox method={this.handleChange} id="pokemonsGene" sortTitle="Sort by Generation" sortType={arrPokemonGene} sortBoxSize="200px" />
+                    <SortBox method={this.handleChange} id="pokemonsType" sortTitle="Sort by Type" sortType={arrPokemonsType} sortBoxSize="160px" />
+                    <input id="pokemonSearch" type="search" onChange={this.handleChange} value={pokemonSearch} placeholder="Search by Name" />
                 </div>
                 <div className="Pokedex">
-                    {pokemons && pokemons.slice(0, search !== "" ? nbPokemons : nbShow)
-                        .filter(pokemon => pokemon.name.toLowerCase().includes(this.state.search.toLowerCase()))
+                    {pokemons && pokemons.slice(0, pokemonSearch !== "" || pokemonsType !== "" || pokemonsGene !== "" ? nbPokemons : nbShow)
+                        .filter(pokemon => pokemon.name.toLowerCase().includes(pokemonSearch.toLowerCase())) //search
+                        .filter(pokemon => pokemonsGene === "1st Generation" ? pokemon.id <= 151 : pokemonsGene === "2nd Generation" ? pokemon.id >= 152 && pokemon.id <= 251 : pokemonsGene === "3rd Generation" ? pokemon.id >= 252 && pokemon.id <= 387 : pokemon) //Sort Generation
+                        .filter(pokemon => pokemon.types[0].type.name.includes(pokemonsType) || pokemon.types[1] &&pokemon.types[1].type.name.includes(pokemonsType)) //Sort Type
                         .map((pokemon, id) => {
                             let pokemonCard =
                                 <div className="pokemon-card" key={id}>
