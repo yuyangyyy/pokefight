4d64ba7ee1d78e8374697f86d3100df1e9f2bc37
Julien Rousseau <julienrousseau16@gmail.com>
2020-04-23 00:09:03 +0200

    transition added + dialogboxes linked (sort of)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Intro from "./components/Intro";
 import Navbar from "./components/Navbar";
 import Potion from "./components/Potion";
 import Ranking from "./components/Ranking";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Transition from "./components/Transition";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import pkball from "./img/logo/Pokeball.png";
 // import Transition from "./components/Transition";
 
 import "./App.css";
@@ -24,7 +27,6 @@ class App extends React.Component {
       <Router>
         <div className="App">
           <Navbar />
-          {/* <Intro /> */}
 
           {/*
         <div className="prez-diagbox">
@@ -56,14 +58,47 @@ class App extends React.Component {
         <Form />
         <Footer /> */}
           <Switch>
-            <Route exact path="/" />
+            <Route exact path="/" component={Intro} />
             <Route path="/new-game">
-              <DialogBox
-                textDialog="Welcome to the world of PokeFight! Start by entering your username"
-                idDialogBox="pseudoPlayer1"
-                firstButton="Confirm"
-                placeholder="Player 1"
-              />
+              <div className="diag-pack">
+                <Link exact to='/new-game-1'><img src={pkball} /></Link>
+                <DialogBox
+                  textDialog="Welcome to the world of PokeFight! Start by entering your username"
+                  idDialogBox="pseudoPlayer1"
+                  firstButton="Confirm"
+                  placeholder="Player 1"
+                />
+              </div>
+            </Route>
+            <Route exact path='/new-game-1'>
+            <div className="diag-pack">
+              <Link exact to='/new-game-2'><img src={pkball} /></Link>
+                <DialogBox
+                  textDialog="Who do you want to fight ?!"
+                  firstButton="Computer"
+                  scndButton="Friend"
+                />
+              </div>
+            </Route>
+            <Route exact path='/new-game-2'>
+            <div className="diag-pack">
+              <Link exact to='/new-game-3'><img src={pkball} /></Link>
+                <DialogBox
+                  textDialog="Enter your friend's name :"
+                  idDialogBox="pseudoPlayer2"
+                  firstButton="Confirm"
+                  placeholder="Player 2"
+                />
+              </div>
+            </Route>
+            <Route exact path='/new-game-3'>
+            <div className="diag-pack">
+              <Link exact to='/new-game-4'><img src={pkball} /></Link>
+                <DialogBox
+                  textDialog="Now, Player1 choose your Pokémon!"
+                  dialogSize="280px"
+                />
+              </div>
             </Route>
             <Route path="/pokedex" component={Potion} />
             <Route path="/ranking" component={Ranking} />
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,13 +2,14 @@ import React from "react";
 import title from "../img/logo/LogoPoke.png";
 import ball from "../img/logo/Pokeball.png";
 import "./Intro.css";
+import {Link} from "react-router-dom"
 
 class Intro extends React.Component {
   render() {
     return (
       <div>
         <img className="titre" src={title} alt="title" />
-        <img className="pokeball" src={ball} alt="pokeball" onClick={this.props.makeVisible} />
+        <Link to='/new-game'><img className="pokeball" src={ball} alt="pokeball" onClick={this.props.makeVisible} /></Link>
       </div>
     );
   }
