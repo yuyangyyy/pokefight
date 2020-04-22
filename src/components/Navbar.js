16f17729aa7e15dee42dab146326020f31903d96
Yang YU <yuyangyyy@gmail.com>
2020-04-22 16:43:36 +0200

    Add menuburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,39 +2,55 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../img/logo/LogoPokeBlanc.png";
 
-function Navbar() {
-  return (
-    <div className="navbar">
-      <div className="sousnavbar">
-        <div className="navlogo">
-          <div className="burger">
-            <span></span>
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  toggleNavbar = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div className={this.state.isOpen ? "navbar isOpen" : "navbar"}>
+        <div className="sousnavbar">
+          <div className="navlogo">
+            <div
+              onClick={this.toggleNavbar}
+              className={this.state.isOpen ? "burger active" : "burger"}
+            >
+              <span></span>
+            </div>
+            <img src={logo} alt="logoPokemon" />
           </div>
-          <img src={logo} alt="logoPokemon" />
-        </div>
 
-        <div className="links">
-          <ul>
-            <li className="new-game">
-              <a href="#Nouvellepartie">New game</a>
-            </li>
+          <div className="links">
+            <ul>
+              <li className="new-game">
+                <a href="#Nouvellepartie">New game</a>
+              </li>
 
-            <li className="pokedex">
-              <a href="#PokéDex">PokéDex</a>
-            </li>
+              <li className="pokedex">
+                <a href="#PokéDex">PokéDex</a>
+              </li>
 
-            <li className="classement">
-              <a href="#Classement">Ranking</a>
-            </li>
+              <li className="classement">
+                <a href="#Classement">Ranking</a>
+              </li>
 
-            <li className="contact">
-              <a href="#Contact">Contact</a>
-            </li>
-          </ul>
+              <li className="contact">
+                <a href="#Contact">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Navbar;
