afdc6c6120c643212a25ab336a082f676f37817c
orko3012 <or.ko@hotmail.fr>
2020-05-11 14:52:49 +0200

    10- add automatik transition

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,45 +1,52 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
 
-import './Transition.css';
-import './Types.css'
+import "./Transition.css";
+import "./Types.css";
 
 class Transition extends React.Component {
+  state = {
+    count: 3,
+  };
 
-	state = {
-		count: 5
-	}
+  redirect = () => {
+    if (this.state.count === 0) {
+      return <Redirect to="/fight" />;
+    }
+  };
 
-	render() {
-		const { count } = this.state
-		const { selectPlayer1, selectPlayer2 } = this.props
-		return (
-			<div className='container' >
-				<div className={`left ${selectPlayer1[0].type}`}>
-					<img src={`https://pokeres.bastionbot.org/images/pokemon/${selectPlayer1[0].id}.png`} />
-				</div>
-				<Link to='/fight'><div className='white'><div id='count'>{count}</div></div></Link>
-				<div className={`right ${selectPlayer2[0].type}`}>
-					<img src={`https://pokeres.bastionbot.org/images/pokemon/${selectPlayer2[0].id}.png`} />
-				</div>
-			</div>
-		)
-	}
-	componentDidMount() {
-		this.countDown =
-			setInterval(() => {
-				if (this.state.count > 0) {
-					this.setState({ count: this.state.count - 1 })
-				}
-			}, 1000)
-	}
+  render() {
+    const { count } = this.state;
+    const { selectPlayer1, selectPlayer2 } = this.props;
+    return (
+      <div className="container">
+        <div className={`left ${selectPlayer1[0].type}`}>
+          <img
+            src={`https://pokeres.bastionbot.org/images/pokemon/${selectPlayer1[0].id}.png`}
+          />
+        </div>
 
-	componentDidUpdate(prevProps, prevState){
-		if(prevState.count === 0){
-			alert("fight")
-		}
-	}
-
+        <Link to="/fight">
+          <div className="white">
+            <div id="count">{count}</div>
+          </div>
+        </Link>
+        <div className={`right ${selectPlayer2[0].type}`}>
+          <img
+            src={`https://pokeres.bastionbot.org/images/pokemon/${selectPlayer2[0].id}.png`}
+          />
+        </div>
+        {this.redirect()}
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.countDown = setInterval(() => {
+      if (this.state.count > 0) {
+        this.setState({ count: this.state.count - 1 });
+      }
+    }, 1000);
+  }
 }
 
-export default Transition
+export default Transition;
